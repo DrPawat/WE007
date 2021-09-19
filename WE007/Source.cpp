e950ec8c5741c064a5bc2870f5e34770ea00801d
PAWAT YAMWONG <64010646@KMITL.AC.TH>
2021-09-19 22:55:22 +0700

    bug

--- WE007/Source.cpp
+++ WE007/Source.cpp
@@ -7,7 +7,66 @@ int main()
 	printf("Roman nuber:");
 	while (num != 0)
 	{
-		if (num >= 1000)
+		if (num == 1000000)
+		{
+			printf("M");
+		}
+		else if (num >= 900000)
+		{
+			printf("CM");
+			num -= 900000;
+		}
+		else if (num >= 500000)
+		{
+			printf("D");
+			num -= 500000;
+		}
+		else if (num >= 400000)
+		{
+			printf("CD");
+			num -= 400000;
+		}
+		else if (num >= 100000)
+		{
+			printf("C");
+			num -= 100000;
+		}
+		else if (num >= 90000)
+		{
+			printf("XC");
+			num -= 90000;
+		}
+		else if (num >= 50000)
+		{
+			printf("L");
+			num -= 50000;
+		}
+		else if (num >= 40000)
+		{
+			printf("XL");
+			num -= 40000;
+		}
+		else if (num >= 10000)
+		{
+			printf("X");
+			num -= 10000;
+		}
+		else if (num >= 9000)
+		{
+			printf("MX");
+			num -= 9000;
+		}
+		else if (num >= 5000)
+		{
+			printf("V");
+			num -= 5000;
+		}
+		else if (num >= 4000)
+		{
+			printf("MV");
+			num -= 4000;
+		}
+		else if (num >= 1000)
 		{
 			printf("M");
 			num -= 1000;
